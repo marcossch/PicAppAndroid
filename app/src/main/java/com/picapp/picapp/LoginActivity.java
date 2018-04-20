1c3b8326c545795de3a9e2d818b9a17fdb8fd703
marcossch <schapiramarcos@gmail.com>
2018-04-20 11:23:27 -0300

    aplicacion con login de mail andando

---
+++ app/src/main/java/com/picapp/picapp/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.picapp.picapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText loginEmailText;
+    private EditText loginPasswordText;
+    private Button loginBtn;
+    private Button loginRegisterBtn;
+
+    private ProgressBar loginProgress;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //instacia de firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        //levanto todos los botones
+        loginEmailText = (EditText) findViewById(R.id.loginEmailText);
+        loginPasswordText = (EditText) findViewById(R.id.loginPasswordText);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        loginRegisterBtn = (Button) findViewById(R.id.loginRegisterBtn);
+        loginProgress = (ProgressBar) findViewById(R.id.loginProgress);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String loginEmail = loginEmailText.getText().toString();
+                String loginPassword = loginPasswordText.getText().toString();
+
+                //si el email y la pass son no vacios
+                if(!TextUtils.isEmpty(loginEmail) && !TextUtils.isEmpty(loginPassword)){
+
+                    //que se ve la barra de progreso
+                    loginProgress.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(loginEmail, loginPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if(task.isSuccessful()){
+
+                                //si se logueo bien lo mando a la actividad principal
+                                sendToMain();
+
+                            } else {
+
+                                //aca hay que manejar el error
+                                String errorMessage = task.getException().getMessage();
+                                Toast.makeText(LoginActivity.this, "Error : " + errorMessage, Toast.LENGTH_LONG).show();
+
+                            }
+
+                            //escondo la barra de progreso
+                            loginProgress.setVisibility(View.INVISIBLE);
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //chequea si el usuario ya esta logueado
+        // si lo esta lo manda a la pagina principal
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            // Si el usuario esta logueado lo mando a la actividad principal
+            sendToMain();
+
+        }
+
+    }
+
+    private void sendToMain() {
+
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+
+    }
+}
--- app/src/main/java/com/picapp/picapp/MainActivity.java
+++ app/src/main/java/com/picapp/picapp/MainActivity.java
@@ -1,16 +1,45 @@
 package com.picapp.picapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //cambiar esto a activity_main
         setContentView(R.layout.activity_main);
-        getWindow().setBackgroundDrawableResource(R.drawable.background);
-        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //esconde el tablero cuando uno abre la app
+        //this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //esconde el tablero cuando uno abre la app
+        //this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        //chequea si el usuario ya esta logueado
+        // si no lo esta lo manda a la pagina de login
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (currentUser != null) {
+            // User is signed in
+        } else {
+            // No user is signed in
+            Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(loginIntent);
+            finish();
+        }
+    }
+
 }
