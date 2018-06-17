d4977792722b863bc966b3ace4efc93918073c14
facundo <facundoiguerrero@gmail.com>
2018-06-17 10:37:35 -0300

    Name button

--- app/src/main/java/com/picapp/picapp/Models/SearchAdapter.java
+++ app/src/main/java/com/picapp/picapp/Models/SearchAdapter.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,7 +24,7 @@ public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.SearchView
 
     class SearchViewHolder extends RecyclerView.ViewHolder{
         ImageView profileImg;
-        TextView name;
+        Button name;
 
         public SearchViewHolder(View itemView) {
             super(itemView);
--- app/src/main/java/com/picapp/picapp/SearchActivity.java
+++ app/src/main/java/com/picapp/picapp/SearchActivity.java
@@ -1,6 +1,5 @@
 package com.picapp.picapp;
 
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,24 +9,16 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
-import com.picapp.picapp.AndroidModels.FeedStory;
 import com.picapp.picapp.Models.SearchAdapter;
 
 import java.util.ArrayList;
