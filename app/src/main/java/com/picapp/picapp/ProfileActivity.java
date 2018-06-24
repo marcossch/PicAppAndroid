3dfb7fa1c24c6b46b0fe1af2e9458d741a846613
marcossch <schapiramarcos@gmail.com>
2018-06-24 13:39:19 -0300

    parche para que no crashee cuando el server no anda

--- app/src/main/java/com/picapp/picapp/ProfileActivity.java
+++ app/src/main/java/com/picapp/picapp/ProfileActivity.java
@@ -192,59 +192,65 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onResponse(Call<UserProfile> call, Response<UserProfile> response) {
 
-                UserProfile userP = response.body();
+                if (response.code() != 200) {
+                    Toast.makeText(ProfileActivity.this, "Error del Server", Toast.LENGTH_LONG).show();
+                } else {
 
-                //levanto la info del usuario
-                name = userP.getName();
-                changeProfileName();
-                picURL = userP.getProfilePic();
-                changeProfilePic();
-                amigos.setText(userP.getNumberOfFriends().toString());
-                publicaciones.setText(userP.getNumberOfStories().toString());
+                    UserProfile userP = response.body();
 
-                List<Story> stories = userP.getStories();
-                for (Story story : stories){
+                    //levanto la info del usuario
+                    name = userP.getName();
+                    changeProfileName();
+                    picURL = userP.getProfilePic();
+                    changeProfilePic();
+                    amigos.setText(userP.getNumberOfFriends().toString());
+                    publicaciones.setText(userP.getNumberOfStories().toString());
 
-                    FeedStory feedStory = new FeedStory();
-                    feedStory.setDescription(story.getDescription());
-                    feedStory.setImage(story.getMedia());
-                    feedStory.setTimestamp(story.getTimestamp());
-                    feedStory.setTitle(story.getTitle());
-                    feedStory.setUser_id(story.getUsername());
-                    feedStory.setProfPic(picURL);
-                    feedStory.setImage_id(story.getStory_id());
-                    feedStory.setName(username);
+                    List<Story> stories = userP.getStories();
+                    for (Story story : stories) {
 
-                    //Diferencio la ubicacon para mostrarla bien y para el mapa
-                    String ubicacion = story.getLocation();
-                    String[] parts = ubicacion.split(",");
-                    String loc = "";
-                    if(parts.length >= 2){
-                        loc += parts[1];
+                        FeedStory feedStory = new FeedStory();
+                        feedStory.setDescription(story.getDescription());
+                        feedStory.setImage(story.getMedia());
+                        feedStory.setTimestamp(story.getTimestamp());
+                        feedStory.setTitle(story.getTitle());
+                        feedStory.setUser_id(story.getUsername());
+                        feedStory.setProfPic(picURL);
+                        feedStory.setImage_id(story.getStory_id());
+                        feedStory.setName(username);
+
+                        //Diferencio la ubicacon para mostrarla bien y para el mapa
+                        String ubicacion = story.getLocation();
+                        String[] parts = ubicacion.split(",");
+                        String loc = "";
+                        if (parts.length >= 2) {
+                            loc += parts[1];
+                        }
+                        if (parts.length >= 3) {
+                            loc += ", " + parts[2];
+                        }
+                        feedStory.setLocation(loc);
+                        if (parts.length > 2) {
+                            String lat = parts[parts.length - 2].substring(10, parts[parts.length - 2].length());
+                            String lng = parts[parts.length - 1].substring(0, parts[parts.length - 1].length() - 1);
+                            latlng = latlng + lat + "," + lng + ";";
+                        }
+
+                        Map<String, String> reactions = story.getReactions();
+                        ArrayList<Comment> coments = story.getComments();
+
+                        if (reactions != null) {
+                            feedStory.setReactions(story.getReactions());
+                        }
+
+                        if (coments != null) {
+                            feedStory.setComments(story.getComments());
+                        }
+
+                        profile_list.add(feedStory);
+                        profileRecyclerAdapter.notifyDataSetChanged();
+
                     }
-                    if(parts.length >= 3){
-                        loc += ", "+parts[2];
-                    }
-                    feedStory.setLocation(loc);
-                    if(parts.length>2) {
-                        String lat = parts[parts.length - 2].substring(10, parts[parts.length - 2].length());
-                        String lng = parts[parts.length - 1].substring(0, parts[parts.length - 1].length() - 1);
-                        latlng = latlng + lat + "," + lng + ";";
-                    }
-
-                    Map<String, String> reactions = story.getReactions();
-                    ArrayList<Comment> coments = story.getComments();
-
-                    if (reactions != null){
-                        feedStory.setReactions(story.getReactions());
-                    }
-
-                    if (coments != null){
-                        feedStory.setComments(story.getComments());
-                    }
-
-                    profile_list.add(feedStory);
-                    profileRecyclerAdapter.notifyDataSetChanged();
 
                 }
 
